0f8a95eb414a71771c42c26fc9742951abaa017a
Sakthiraj <Sakthiraj.Kanakarathinam@i-med.com.au>
2019-09-12 12:19:30 +1000

    Resolved constant file.

--- referrer-portal/src/main/java/au/com/imed/portal/referrer/referrerportal/common/PortalConstant.java
+++ referrer-portal/src/main/java/au/com/imed/portal/referrer/referrerportal/common/PortalConstant.java
@@ -34,12 +34,7 @@ public final class PortalConstant {
 	public static final String DISPLAY_DATE_FORMAT = "dd/MM/yyyy";
 	public static final String DISPLAY_DATE_TIME_FORMAT = "dd/MM/yyyy HH:mm:ss";
 
+	// Approver
+	public static final String PARAM_ATTR_ACC_LOCKED = "ibm-pwdAccountLocked";
 
-  // Temporal password for COMRAD
-  public static String IMED_TEMPORAL_PASSWORD = "iMtmP2018pswd";
-  
-  // Approver
-  public static final String PARAM_ATTR_ACC_LOCKED = "ibm-pwdAccountLocked";
-  public static final String PARAM_ATTR_FINALIZING_PAGER = "pager";
-  public static final String PARAM_ATTR_VALUE_FINALIZING_PAGER = "Finalizing";
 }
