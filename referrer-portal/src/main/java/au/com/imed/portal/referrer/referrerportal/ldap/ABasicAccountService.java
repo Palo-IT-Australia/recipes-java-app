c5a28f145f575b4f16bf626fda6c9a507ec8f0cf
Sakthiraj <Sakthiraj.Kanakarathinam@i-med.com.au>
2019-09-12 13:40:50 +1000

    Refactor code for duplicates.

--- referrer-portal/src/main/java/au/com/imed/portal/referrer/referrerportal/ldap/ABasicAccountService.java
+++ referrer-portal/src/main/java/au/com/imed/portal/referrer/referrerportal/ldap/ABasicAccountService.java
@@ -1,75 +1,101 @@
 package au.com.imed.portal.referrer.referrerportal.ldap;
 
+import java.util.Map;
+
 import javax.naming.Name;
 import javax.naming.NamingException;
 import javax.naming.directory.Attributes;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.ldap.core.AttributesMapper;
 import org.springframework.ldap.core.DirContextOperations;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.ldap.core.support.AbstractContextMapper;
 import org.springframework.ldap.core.support.LdapContextSource;
+import org.springframework.web.client.RestTemplate;
 
 import au.com.imed.portal.referrer.referrerportal.common.PortalConstant;
+import au.com.imed.portal.referrer.referrerportal.common.util.IgnoreCertFactoryUtil;
 import au.com.imed.portal.referrer.referrerportal.model.AccountDetail;
 import au.com.imed.portal.referrer.referrerportal.model.LdapUserDetails;
 import au.com.imed.portal.referrer.referrerportal.model.StageUser;
 
 public abstract class ABasicAccountService {
-	@Value("${imed.ldap.url}")
-	private String LDAP_URL;
 
-	protected final static String DOMAIN_REFERRER = "ou=Referrers,ou=Portal,ou=Applications,dc=mia,dc=net,dc=au";
-	protected final static String DOMAIN_STAGING = "ou=Staging,ou=Portal,ou=Applications,dc=mia,dc=net,dc=au";
-			
+	@Value("${imed.ldap.url}")
+	private String ldapUrl;
+
+	@Value("${imed.ldap.userdn}")
+	private String ldapUserDn;
+
+	@Value("${imed.ldap.password}")
+	private String ldapPassword;
+
+	//
+	// Rest
+	//
+	public <S> ResponseEntity<S> doGet(String url, Map<String, String> requestParams, Class<S> clz) {
+		HttpComponentsClientHttpRequestFactory factory = null;
+		try {
+			factory = IgnoreCertFactoryUtil.createFactory();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		RestTemplate restTemplate = (factory == null) ? new RestTemplate() : new RestTemplate(factory);
+		return restTemplate.getForEntity(url, clz, requestParams);
+	}
+
 	//
 	// LDAP
 	//
 	protected LdapTemplate getReferrerLdapTemplate() throws Exception {
-		return getLdapTemplate(DOMAIN_REFERRER);
+		return getLdapTemplate(PortalConstant.DOMAIN_REFERRER);
 	}
 
 	protected LdapTemplate getReferrerStagingLdapTemplate() throws Exception {
-		return getLdapTemplate("ou=Staging,ou=Portal,ou=Applications,dc=mia,dc=net,dc=au");
+		return getLdapTemplate(PortalConstant.DOMAIN_STAGING);
 	}
-
 
 	protected LdapTemplate getPortalLdapTemplate() throws Exception {
-		return getLdapTemplate("ou=Portal,ou=Applications,dc=mia,dc=net,dc=au");
+		return getLdapTemplate(PortalConstant.DOMAIN_PORTAL);
 	}
-	
+
+	protected LdapTemplate getPatientLdapTemplate() throws Exception {
+		return getLdapTemplate(PortalConstant.DOMAIN_PATIENTS);
+	}
 
 	protected LdapTemplate getApplicationsLdapTemplate() throws Exception {
-		return getLdapTemplate("ou=Applications,dc=mia,dc=net,dc=au");
+		return getLdapTemplate(PortalConstant.DOMAIN_APPLICATIONS);
 	}
 
 	protected LdapTemplate getGlobalLdapTemplate() throws Exception {
-		return getLdapTemplate("dc=mia,dc=net,dc=au");
+		return getLdapTemplate(PortalConstant.DOMAIN_GLOBAL);
 	}
 
 	protected LdapTemplate getBusinessUnitLdapTemplate() throws Exception {
-		return getLdapTemplate("ou=Business Units,dc=mia,dc=net,dc=au");
-	}
-
-	protected LdapTemplate getImedPacsLdapTemplate() throws Exception {
-		return getLdapTemplate("ou=IMED PACS Users,dc=mia,dc=net,dc=au");
-	}
-
-	protected LdapTemplate getPacsLdapTemplate() throws Exception {
-		return getLdapTemplate("ou=PACS Users,dc=mia,dc=net,dc=au");
+		return getLdapTemplate(PortalConstant.DOMAIN_BUSINESS_UNITS);
 	}
 
 	protected LdapTemplate getStagePacsLdapTemplate() throws Exception {
-		return getLdapTemplate("ou=Staging PACS Users,dc=mia,dc=net,dc=au");
+		return getLdapTemplate(PortalConstant.DOMAIN_STAGING_PACS_USERS);
+	}
+
+	protected LdapTemplate getImedPacsLdapTemplate() throws Exception {
+		return getLdapTemplate(PortalConstant.DOMAIN_IMED_PACS_USERS);
+	}
+
+	protected LdapTemplate getPacsLdapTemplate() throws Exception {
+		return getLdapTemplate(PortalConstant.DOMAIN_PACS_USERS);
 	}
 
 	private LdapTemplate getLdapTemplate(final String baseDomain) throws Exception {
 		LdapContextSource contextSource = new LdapContextSource();
 		contextSource.setBase(baseDomain);
-		contextSource.setUserDn("cn=root");
-		contextSource.setPassword("794bo3HAST");
-		contextSource.setUrl(LDAP_URL);
+		contextSource.setUserDn(ldapUserDn);
+		contextSource.setPassword(ldapPassword);
+		contextSource.setUrl(ldapUrl);
 		contextSource.afterPropertiesSet();
 
 		return new LdapTemplate(contextSource);
@@ -110,7 +136,7 @@ public abstract class ABasicAccountService {
 			return detail;
 		}
 	}
-	
+
 	protected class LdapUserDetailsUserAttributeMapper implements AttributesMapper<LdapUserDetails> {
 		@Override
 		public LdapUserDetails mapFromAttributes(Attributes attrs) throws NamingException {
@@ -122,9 +148,12 @@ public abstract class ABasicAccountService {
 			detail.setUid(attrs.get("uid") != null ? attrs.get("uid").get(0).toString() : "");
 			detail.setCn(attrs.get("cn") != null ? attrs.get("cn").get(0).toString() : "");
 			detail.setAhpra(attrs.get("ahpra") != null ? attrs.get("ahpra").get(0).toString() : "");
-			detail.setLocked(attrs.get(PortalConstant.PARAM_ATTR_ACC_LOCKED) != null ? "true".equals(attrs.get(PortalConstant.PARAM_ATTR_ACC_LOCKED).get(0).toString()) : false);
+			detail.setLocked(attrs.get(PortalConstant.PARAM_ATTR_ACC_LOCKED) != null
+					? "true".equals(attrs.get(PortalConstant.PARAM_ATTR_ACC_LOCKED).get(0).toString())
+					: false);
 			detail.setAccountType(attrs.get("employeeType") != null ? attrs.get("employeeType").get(0).toString() : "");
-			detail.setBusinessUnit(attrs.get("BusinessUnit") != null ? attrs.get("BusinessUnit").get(0).toString() : "");
+			detail.setBusinessUnit(
+					attrs.get("BusinessUnit") != null ? attrs.get("BusinessUnit").get(0).toString() : "");
 			return detail;
 		}
 	}
