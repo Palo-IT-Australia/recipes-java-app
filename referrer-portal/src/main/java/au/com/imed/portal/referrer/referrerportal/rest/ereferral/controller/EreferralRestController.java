70d41836c2589df2f1ce2d729151816114a43f0b
huehara <Hidehiro.Uehara@i-med.com.au>
2019-10-21 15:27:30 +1100

    reauth in rest controller

--- referrer-portal/src/main/java/au/com/imed/portal/referrer/referrerportal/rest/ereferral/controller/EreferralRestController.java
+++ referrer-portal/src/main/java/au/com/imed/portal/referrer/referrerportal/rest/ereferral/controller/EreferralRestController.java
@@ -1,13 +1,17 @@
 package au.com.imed.portal.referrer.referrerportal.rest.ereferral.controller;
 
 import org.jose4j.json.internal.json_simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import au.com.imed.common.active.directory.manager.ImedActiveDirectoryLdapManager;
+import au.com.imed.portal.referrer.referrerportal.ldap.ReferrerAccountService;
 import au.com.imed.portal.referrer.referrerportal.rest.ereferral.model.EreferralEreferral;
 import au.com.imed.portal.referrer.referrerportal.rest.ereferral.model.EreferralReauth;
 import ca.uhn.hl7v2.DefaultHapiContext;
@@ -20,27 +24,34 @@ import ca.uhn.hl7v2.parser.Parser;
 @RestController
 @RequestMapping("/ereferralrest")
 public class EreferralRestController {
+	@Autowired
+	private ReferrerAccountService referrerAccountService;
+	
 	@SuppressWarnings("unchecked")
 	@PostMapping("/reauth")
-	public ResponseEntity<JSONObject> postReauth(@RequestBody() EreferralReauth reauth) {
-		// TODO check pswd by ldaptemp
+	public ResponseEntity<JSONObject> postReauth(@RequestBody() EreferralReauth reauth, Authentication authentication) {
+		boolean authed = referrerAccountService.checkPassword(authentication.getName(), reauth.getPassword())
+			|| new ImedActiveDirectoryLdapManager().checkPassword(authentication.getName(), reauth.getPassword());
 		JSONObject reps = new JSONObject();
-		reps.put("msg", "success");
-		return ResponseEntity.status(HttpStatus.OK).body(reps);
+		reps.put("msg", authed ? "success" : "failure");
+		return ResponseEntity.status(authed ? HttpStatus.OK : HttpStatus.UNAUTHORIZED).body(reps);
 	}
 
 	@SuppressWarnings("unchecked")
 	@PostMapping("/referral")
-	public ResponseEntity<JSONObject> postEreferral(@RequestBody() EreferralEreferral referral) {
-		// TODO HL7 or API
+	public ResponseEntity<JSONObject> postEreferral(@RequestBody() EreferralEreferral referral, Authentication authentication) {
 		JSONObject reps = new JSONObject();
-		reps.put("msg", "success");
-		try {
-			reps.put("hlseven", processReferral(referral));
-		} catch (Exception e) {
-			e.printStackTrace();
+		if(authentication != null && authentication.getName() != null) {
+			// TODO HL7 or API
+			reps.put("msg", "success");
+			try {
+				reps.put("hlseven", processReferral(referral));
+				return ResponseEntity.status(HttpStatus.OK).body(reps);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
-		return ResponseEntity.status(HttpStatus.OK).body(reps);
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(reps);
 	}
 
 	private String processReferral(final EreferralEreferral referral) throws Exception {
