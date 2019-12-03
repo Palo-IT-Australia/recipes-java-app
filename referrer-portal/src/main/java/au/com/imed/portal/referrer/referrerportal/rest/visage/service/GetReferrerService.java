7c1ce5c8688396301da3854cbf2afbbff57e4687
huehara <Hidehiro.Uehara@i-med.com.au>
2019-12-04 10:14:01 +1100

    ahpra and provnum method

--- referrer-portal/src/main/java/au/com/imed/portal/referrer/referrerportal/rest/visage/service/GetReferrerService.java
+++ referrer-portal/src/main/java/au/com/imed/portal/referrer/referrerportal/rest/visage/service/GetReferrerService.java
@@ -11,10 +11,25 @@ import au.com.imed.portal.referrer.referrerportal.rest.visage.model.Referrer;
 @Service
 public class GetReferrerService extends AVisageRestClientService<Map<String, String>, Referrer> {
   public static final String PARAM_CURRENT_USER_NAME = "currentUserName";
+  public static final String PARAM_PROVIDER_NUMBER = "providerNumber";
+  public static final String PARAM_AHPRA_NUMBER = "ahpraNumber";
 
   @Override
   protected String getCommandPath(Map<String, String> requestParams) {
-    return "/referrer/" + requestParams.get(PARAM_CURRENT_USER_NAME);
+  	String path;
+  	if(requestParams.containsKey(PARAM_PROVIDER_NUMBER)) 
+  	{
+  		path = "providerNumber/" + requestParams.get(PARAM_PROVIDER_NUMBER);
+  	}
+  	else if(requestParams.containsKey(PARAM_AHPRA_NUMBER)) 
+  	{
+  		path = "ahpraNumber/" + requestParams.get(PARAM_AHPRA_NUMBER);
+  	}
+  	else
+  	{
+  		path = requestParams.get(PARAM_CURRENT_USER_NAME);
+  	}
+    return "/referrer/" + path;
   }
 
   @Override
