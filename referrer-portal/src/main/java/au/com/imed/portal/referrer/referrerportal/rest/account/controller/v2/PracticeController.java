3b95ff89dbe1e72a2649815e45a226e5eb086458
callumlawson-smith <clawsonsmith@au.palo-it.com>
2021-06-29 10:46:03 +1000

    basic setup

--- referrer-portal/src/main/java/au/com/imed/portal/referrer/referrerportal/rest/account/controller/v2/PracticeController.java
+++ referrer-portal/src/main/java/au/com/imed/portal/referrer/referrerportal/rest/account/controller/v2/PracticeController.java
@@ -1,19 +1,39 @@
 package au.com.imed.portal.referrer.referrerportal.rest.account.controller.v2;
 
+import au.com.imed.portal.referrer.referrerportal.email.ReferrerMailService;
+import au.com.imed.portal.referrer.referrerportal.model.AddPractice;
 import au.com.imed.portal.referrer.referrerportal.rest.models.ErrorResponse;
+import au.com.imed.portal.referrer.referrerportal.security.DetailedLdapUserDetails;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("${imed.api-v2.prefix}/practice")
 @PreAuthorize("isAuthenticated()")
+@Slf4j
 public class PracticeController {
+
+    @Autowired
+    private ReferrerMailService emailService;
+
     @PostMapping("/add")
-    public ResponseEntity<ErrorResponse> addPractice () {
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    public ResponseEntity<ErrorResponse> addPractice (@RequestBody AddPractice practice, final Authentication authentication) {
+        log.info("/practice/add" + practice.toString());
+        log.info(authentication.toString());
+        try {
+            DetailedLdapUserDetails principal = (DetailedLdapUserDetails) authentication.getPrincipal();
+            emailService.sendAddPractice(practice, principal);
+            return new ResponseEntity<>(HttpStatus.ACCEPTED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 }
---
+++ referrer-portal/src/main/java/au/com/imed/portal/referrer/referrerportal/rest/account/model/Practice.java
@@ -0,0 +1,82 @@
+package au.com.imed.portal.referrer.referrerportal.rest.account.model;
+
+public class Practice {
+    private String providerNumber;
+    private String name;
+    private String phone;
+    private String fax;
+    private String street;
+    private String suburb;
+    private String state;
+    private String postcode;
+
+    public String getProviderNumber() {
+        return providerNumber;
+    }
+
+    public void setProviderNumber(String providerNumber) {
+        this.providerNumber = providerNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getSuburb() {
+        return suburb;
+    }
+
+    public void setSuburb(String suburb) {
+        this.suburb = suburb;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    @Override
+    public String toString() {
+        return "Practice [providerNumber=" + providerNumber + ", name=" + name + ", phone=" + phone + ", fax=" + fax
+                + ", street=" + street + ", suburb=" + suburb + ", state=" + state + ", postcode=" + postcode + "]";
+    }
+}
