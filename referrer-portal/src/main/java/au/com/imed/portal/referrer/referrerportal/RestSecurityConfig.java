110849ef57676a914bebae532645e795156feeee
Janis Ardel <jardel@au.palo-it.com>
2021-08-31 17:47:25 +1000

    separate RestSecurityConfig from mvc security config

---
+++ referrer-portal/src/main/java/au/com/imed/portal/referrer/referrerportal/RestSecurityConfig.java
@@ -0,0 +1,26 @@
+package au.com.imed.portal.referrer.referrerportal;
+
+import au.com.imed.portal.referrer.referrerportal.jwt.JwtTokenFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@Order(2)
+public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/api/v2/**")
+                .authorizeRequests()
+                .antMatchers("/api/v2/**").permitAll()
+                .anyRequest().fullyAuthenticated()
+                .and().cors()
+                .and().addFilterAfter(new JwtTokenFilter(), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .authenticationEntryPoint(new Http403ForbiddenEntryPoint()).and();
+    }
+}
