6569b3292febb20bee8c25d992bc372bad374e19
Janis Ardel <jardel@au.palo-it.com>
2021-09-01 22:56:04 +1000

    refactor rest security config out from mvc security config

--- referrer-portal/src/main/java/au/com/imed/portal/referrer/referrerportal/RestSecurityConfig.java
+++ referrer-portal/src/main/java/au/com/imed/portal/referrer/referrerportal/RestSecurityConfig.java
@@ -1,26 +1,35 @@
 package au.com.imed.portal.referrer.referrerportal;
 
 import au.com.imed.portal.referrer.referrerportal.jwt.JwtTokenFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@Order(2)
+@Order(1)
+@EnableWebSecurity
 public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${imed.api-v2.prefix}/**")
+    private String apiV2Prefix;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.antMatcher("/api/v2/**")
+        http.antMatcher(apiV2Prefix)
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeRequests()
-                .antMatchers("/api/v2/**").permitAll()
+                .antMatchers(apiV2Prefix).permitAll()
+                .antMatchers(apiV2Prefix).permitAll()
                 .anyRequest().fullyAuthenticated()
-                .and().cors()
-                .and().addFilterAfter(new JwtTokenFilter(), UsernamePasswordAuthenticationFilter.class)
-                .exceptionHandling()
-                .authenticationEntryPoint(new Http403ForbiddenEntryPoint());
+                .and()
+                .addFilterAfter(new JwtTokenFilter(), UsernamePasswordAuthenticationFilter.class)
+                .csrf().disable();
     }
 }
